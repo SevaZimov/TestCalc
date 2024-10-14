be368fcc285df2e0f87b83a11ec158160c5661ac
SevaZimov <zimovetsva@gmail.com>
2024-10-14 10:50:55 +0400

    Добавили функцию целочисленного деления

--- main.py
+++ main.py
@@ -13,5 +13,11 @@ def minus(a,b):
 def multipl(a,b):
     return a*b
 
+
+def div(a,b):
+    if (type(a)==int and type(b)==int):
+        return(a//b, a%b)
+
 if __name__ == "__main__":
     print(plus(1,2))
+    print(div(4,3))
